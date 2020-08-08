8c3b115373dec792f78eb532bffc77cc148db29e
Ketan Sahu <ketankumar120@gmail.com>
2020-08-08 15:13:29 +0530

    static folder added

--- LAHI/teachers/models.py
+++ LAHI/teachers/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from main.models import *
 
 class Student(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.ForeignKey(User, on_delete=models.CASCADE)
     roll_no = models.IntegerField()
     std = models.ForeignKey(Standard, default=None, on_delete=models.SET_DEFAULT)
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
