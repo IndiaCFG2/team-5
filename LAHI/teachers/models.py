3a389de11d5a05526e779ddf32500ce27b463a28
Ketan Sahu <ketankumar120@gmail.com>
2020-08-08 21:20:13 +0530

    modified student and teacher table

--- LAHI/teachers/models.py
+++ LAHI/teachers/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from main.models import *
+from django.contrib.auth.models import User
 
 class Student(models.Model):
-    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, default=None, on_delete=models.CASCADE)
     roll_no = models.IntegerField()
+    phone = models.CharField(max_length=15, default=None)
     std = models.ForeignKey(Standard, default=None, on_delete=models.SET_DEFAULT)
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return str(self.user)
