c9009b26301ca6ca99e0a1d33264ca46fac339da
Ketan Sahu <ketankumar120@gmail.com>
2020-08-08 22:06:52 +0530

    student creation form created

--- LAHI/teachers/forms.py
+++ LAHI/teachers/forms.py
@@ -2,21 +2,22 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User 
 from main.models import MediaFile
+from .models import Student
 
 
-class NewStudentForm(UserCreationForm):
+class NewStudentForm(forms.ModelForm):
 	# email = forms.EmailField(required=True)
     
 	class Meta:
-		model = User
-		fields = ('username', 'password1', 'password2')
+		model = Student
+		fields = ['name', 'roll_no', 'phone', 'std']
 
-	def save(self, commit=True):
-		user = super(NewStudentForm, self).save(commit=False)
-		# user.email = self.cleaned_data['email']
-		if commit:
-			user.save()
-		return user
+	# def save(self, commit=True):
+	# 	user = super(NewStudentForm, self).save(commit=False)
+	# 	user.email = self.cleaned_data['email']
+	# 	if commit:
+	# 		user.save()
+	# 	return user
 
 class MediaForm(forms.ModelForm):
     class Meta:
--- LAHI/teachers/models.py
+++ LAHI/teachers/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from main.models import *
-from django.contrib.auth.models import User
+
 
 class Student(models.Model):
-    user = models.OneToOneField(User, default=None, on_delete=models.CASCADE)
-    roll_no = models.IntegerField()
+    name = models.CharField(max_length=100, default=None)
+    roll_no = models.IntegerField(unique=True)
     phone = models.CharField(max_length=15, default=None)
     std = models.ForeignKey(Standard, default=None, on_delete=models.SET_DEFAULT)
 
     def __str__(self):
-        return str(self.user)
+        return self.name
