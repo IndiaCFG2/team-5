ec35b3ee32fe9fb362a653bdc9c3ec8c42b25e3a
Ketan Sahu <49114143+ketansahu07@users.noreply.github.com>
2020-08-09 01:40:22 +0530

    Update views.py

--- LAHI/main/views.py
+++ LAHI/main/views.py
@@ -3,10 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def homepage(request):
-<<<<<<< HEAD
     return render(request, 'main/homepage.html')    
-=======
-    return render(request, 'main\homepage.html')
-def loginS(request):
-    return render(request, 'login.html')    
->>>>>>> 573eda1abcbf58a500a0da2be3195105e79b96d6
